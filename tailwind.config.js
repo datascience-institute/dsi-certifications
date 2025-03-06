d080161c815ce9341bfcab41bf5e63761db3e523
datascience-institute <grainne@datascienceinstitute.net>
2025-03-06 15:54:21 +0000

    Update tailwind.config.js
    I changed all occurrences of indigo to amber

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
@@ -13,8 +14,23 @@ module.exports = {
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))"
-      }
-    }
+      },
+      // Overwrite the "indigo" palette with amber
+      colors: {
+        indigo: {
+          50: colors.amber[50],
+          100: colors.amber[100],
+          200: colors.amber[200],
+          300: colors.amber[300],
+          400: colors.amber[400],
+          500: colors.amber[500],
+          600: colors.amber[600],
+          700: colors.amber[700],
+          800: colors.amber[800],
+          900: colors.amber[900],
+        },
+      },
+    },
   },
   variants: {
     extend: {}
