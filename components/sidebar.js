0ada93d4e6066577346c1188e62b18500c5ed8f9
datascience-institute <grainne@datascienceinstitute.net>
2025-03-06 16:10:51 +0000

    Update sidebar.js
    violet to yellow

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,17 +1,19 @@
 import { cx } from "@utils/all";
 import Link from "next/link";
+
 export default function Sidebar(props) {
   return (
-    <div className="sticky h-[calc(100vh-55px)] border-r  border-gray-200 top-[55px] hidden md:block">
+    <div className="sticky h-[calc(100vh-55px)] border-r border-gray-200 top-[55px] hidden md:block">
       <SideNav items={props.items} active={props.active} />
-      <div className="border-t border-gray-200 p-4 absolute bottom-0 w-full ">
+      <div className="border-t border-gray-200 p-4 absolute bottom-0 w-full">
         <p className="text-sm text-slate-500">
           Made by{" "}
           <a
             href="https://web3templates.com"
             target="_blank"
-            className="text-violet-500 hover:text-violet-600"
-            rel="noopener">
+            rel="noopener"
+            className="text-yellow-500 hover:text-yellow-600"
+          >
             Web3Templates
           </a>
         </p>
@@ -24,27 +26,29 @@ export function SideNav(props) {
   return (
     <div
       className={cx(
-        "lg:w-64 pb-5  px-5 overflow-y-auto ",
-        !props.mobile && " h-[calc(100%-53px)] "
-      )}>
+        "lg:w-64 pb-5 px-5 overflow-y-auto",
+        !props.mobile && "h-[calc(100%-53px)]"
+      )}
+    >
       <ul>
         {props.items.map((item, index) => (
           <li key={index} className="mt-5">
-            <span className=" font-semibold"> {item.title}</span>
+            <span className="font-semibold">{item.title}</span>
             <ul className="mt-2 mx-4">
               {item.items.map((doc, docindex) => (
                 <li key={docindex}>
                   <Link
-                    href={`/docs/${item.slug.current}/${doc.slug.current}`}>
+                    href={`/docs/${item.slug.current}/${doc.slug.current}`}
+                  >
                     <a
                       onClick={props.closeModal}
                       className={cx(
-                        "inline-flex py-1 text-sm  hover:text-violet-500 focus:text-violet-500",
+                        "inline-flex py-1 text-sm hover:text-yellow-500 focus:text-yellow-500",
                         doc.slug.current === props.active
-                          ? "text-violet-500"
+                          ? "text-yellow-500"
                           : "text-slate-600"
-                      )}>
-                      {" "}
+                      )}
+                    >
                       {doc.title}
                     </a>
                   </Link>
